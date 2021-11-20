36389cf918ec3ca3dc5f1b241ede1ef6bcdb6618
Julia Qian <qjulia32@gmail.com>
2021-11-19 17:51:50 -0800

    testing

--- scanpy/external/tl/_gsea.py
+++ scanpy/external/tl/_gsea.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 def gsea_barplot(
     df: pd.DataFrame, 
-    score: Optional['str'] = 'pval',
+    score: Optional['str'] = 'nes',
     score_cutoff: Optional[float] = None,
     top_term: Optional[float] = 10,
     figsize: Optional[Tuple[float, float]] = (6.5,6),
@@ -27,7 +27,7 @@ def gsea_barplot(
     df
         Dataframe where each row is an enrichment term with GSEA scores as columns
     score
-        Chosen score to display.
+        Chosen score to display. Default is 'nes'
         Must be exact name of column as it appears in dataframe.
         gseapy supports: 'es', 'nes', 'pval', 'fdr'
         fgsea supports: 'es', 'nes', 'pval', 'padj', 'log2error'
@@ -75,6 +75,7 @@ def gsea_barplot(
       ax = fig.add_subplot(111)
 
       # user defined colors for positive and negative values
+      colors = color
       if (dd[score] > 0).any() and (dd[score] < 0).any():
         if len(color) == 2:
           color_tmp = ['']*len(dd[score])
@@ -83,12 +84,12 @@ def gsea_barplot(
               color_tmp[i] = color[0]
             else:
               color_tmp[i] = color[1]
-          color = color_tmp
+          colors = color_tmp
 
       bar = dd.plot.barh( 
           y=score,
           alpha=0.75, 
-          color = color,
+          color = colors,
           fontsize=16, 
           ax=ax)
       
@@ -98,16 +99,17 @@ def gsea_barplot(
       bar.set_ylabel(ylabel, fontsize=16, fontweight='bold')
 
       bar.legend_.remove()
-      bar.set_title(title, fontsize=24, fontweight='bold')
+      bar.set_title(title, fontsize=16, fontweight='bold')
 
       plt.savefig(out_dir)     
+      
+      return ax
 
     except KeyError:
       print('ERROR: key \'' + str(score) + '\' is not in provided dataframe')
 
 def test ():
     import rpy2
-
     import rpy2.robjects as robjects
     from rpy2.robjects.packages import importr
     base = importr('base')
@@ -122,5 +124,10 @@ def test ():
 def testR ():
     import subprocess
     # rcode = subprocess("./test.R")
-
     subprocess.call (["C:/Program Files/R/R-4.1.2/bin/Rscript", "--vanilla", "C:/Users/qjuli/Downloads/Harvey Mudd/Clinic/scanpy/scanpy/external/tl/test.R"])
+
+def test_plot():
+    from matplotlib.testing.compare import compare_images
+    gseapy_df = pd.read_csv('.\gseapy_sample_df.csv')
+    gsea_barplot(gseapy_df, score = 'nes', color = ['b','r'], out_dir = 'gsea_test.png')
+    assert compare_images('gsea_test.png', 'gseabarplot.png', tol=5) is None
