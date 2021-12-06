a09ff10a2e9a45f312d16c7b5f301d73265147ce
Julia Qian <qjulia32@gmail.com>
2021-12-06 14:18:44 -0800

    cleaning gseabarplot branch

--- scanpy/tests/external/test_gsea.py
+++ scanpy/tests/external/test_gsea.py
@@ -11,6 +11,6 @@ def test_gsea_barplot():
     """
     Test that gsea_barplot works
     """
-    gseapy_df = pd.read_csv('.\data\gseapy_sample_df.csv')
+    gseapy_df = pd.read_csv('.\_data\gseapy_sample_df.csv')
     sce.tl.gsea_barplot(gseapy_df, score = 'nes', color = ['b','r'], out_dir = 'gsea_test.png')
-    assert compare_images('gsea_test.png', '.\data\gseabarplot.png', tol=5) is None
+    assert compare_images('gsea_test.png', '.\_images\master_gsea_barplot.png', tol=5) is None
