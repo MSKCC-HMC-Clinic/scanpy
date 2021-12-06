37174ff22ccdfd8eb9cd57cc352dd8fe92717607
Julia Qian <qjulia32@gmail.com>
2021-12-06 14:09:09 -0800

    added documentation

--- scanpy/external/tl/_execute_r_script.py
+++ scanpy/external/tl/_execute_r_script.py
@@ -7,7 +7,33 @@ import subprocess, os
 
 HERE = Path(__file__).parent 
 
-def testR (r_filename="test.R", rscript_command_path=None):
+def testR (
+    r_filename: str = 'test.R'
+):
+    """\
+    
+    Parameters
+    ----------
+    r_filename
+        Filename of R script to run
+   
+    Returns
+    -------
+    Boolean of whether script ran successfully
+    
+    Notes
+    -------
+	This function requires the local path to the Rscript command to run the 
+    provided script using the subprocess module
+
+    This function will prompt the user in terminal for the Rscript command path
+
+    Example
+    -------
+    >>> is_success = scanpy.external.tl.execute_r_script('test.R')
+ 
+    """
+ 
     path2rscript = Path(HERE, r_filename)
     command = "/Library/Frameworks/R.framework/Versions/3.6/Resources/Rscript"
     arg = '--vanilla'
