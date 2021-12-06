f1603992a2260458199b21c73b518edb5a1cd662
Julia Qian <qjulia32@gmail.com>
2021-12-06 13:40:11 -0800

    cleaning up branch

--- scanpy/external/tl/_execute_r_script.py
+++ scanpy/external/tl/_execute_r_script.py
@@ -7,8 +7,8 @@ import subprocess, os
 
 HERE = Path(__file__).parent 
 
-def testR (rscript="test.R"):
-    path2rscript = Path(HERE, rscript)
+def testR (r_filename="test.R", rscript_command_path=None):
+    path2rscript = Path(HERE, r_filename)
     command = "/Library/Frameworks/R.framework/Versions/3.6/Resources/Rscript"
     arg = '--vanilla'
     subprocess.call ([command, arg, path2rscript])
