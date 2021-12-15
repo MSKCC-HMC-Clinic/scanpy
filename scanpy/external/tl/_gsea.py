5fd70a23835c456a0b1673423945d20f1afdccfc
Julia Qian <qjulia32@gmail.com>
2021-12-15 15:31:06 -0800

    flake8 errors

--- scanpy/external/tl/_gsea.py
+++ scanpy/external/tl/_gsea.py
@@ -1,11 +1,12 @@
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Union
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import scanpy.external as sce
 
+
 def gsea_barplot(
-    df: pd.DataFrame, 
+    df: pd.DataFrame,
     score: Optional['str'] = 'nes',
     score_cutoff: Optional[float] = None,
     top_term: Optional[float] = 10,
@@ -14,15 +15,14 @@ def gsea_barplot(
     x_label: Optional['str'] = None,
     y_label: Optional['str'] = None,
     out_dir: Optional['str'] = 'gseabarplot.png',
-    color = '#1f77b4', # matplotlib default: https://matplotlib.org/stable/users/dflt_style_changes.html#id1
+    color: Optional[Union['str',list]] = '#1f77b4',
 ):
-  
+
     """\
     Horizontal barplot of GSEA results of genes by enrichment terms given a dataframe
-
     Allows visualizations of either pval, nes, and fdr from outputs using either gseapy or fgsea.
     Dataframe likely generated from scanpy.external.tl.gsea() function.
-    
+
     Parameters
     ----------
     df
@@ -52,12 +52,13 @@ def gsea_barplot(
     -------
     Returns horizontal barplot with a bar for each of the top enrichment terms
     by the chosen score
-    
+
     Notes
     -------
     Is designed to take in output df from scanpy.external.tl.gsea() function.
-    The score supported is dependent on the type of gsea previously run, 
+    The score supported is dependent on the type of gsea previously run,
     either 'gseapy' or 'fgsea'
+    Default color is matplotlib default: https://matplotlib.org/stable/users/dflt_style_changes.html#id1
 
     Example
     -------
@@ -66,48 +67,49 @@ def gsea_barplot(
 
     """
     try:
-      dd = df.sort_values(by=score)
-      fig = plt.figure(figsize=figsize)
-      dd = df.sort_values(by=score).iloc[-top_term:,:]
+        dd = df.sort_values(by=score)
+        fig = plt.figure(figsize=figsize)
+        dd = df.sort_values(by=score).iloc[-top_term:,:]
 
-      if score_cutoff:
-        dd = dd[dd[score] > score_cutoff]  
+        if score_cutoff:
+            dd = dd[dd[score] > score_cutoff]
 
-      ax = fig.add_subplot(111)
+        ax = fig.add_subplot(111)
 
-      # user defined colors for positive and negative values
-      colors = color
-      if (dd[score] > 0).any() and (dd[score] < 0).any():
-        if len(color) == 2:
-          color_tmp = ['']*len(dd[score])
-          for i, s in enumerate(dd[score]):
-            if s >= 0:
-              color_tmp[i] = color[0]
-            else:
-              color_tmp[i] = color[1]
-          colors = color_tmp
+        # user defined colors for positive and negative values
+        colors = color
+        if (dd[score] > 0).any() and (dd[score] < 0).any():
+            if len(color) == 2:
+                color_tmp = [''] * len(dd[score])
+            for i, s in enumerate(dd[score]):
+                if s >= 0:
+                    color_tmp[i] = color[0]
+                else:
+                    color_tmp[i] = color[1]
+            colors = color_tmp
 
-      bar = dd.plot.barh( 
-          y=score,
-          alpha=0.75, 
-          color = colors,
-          fontsize=16, 
-          ax=ax)
-      
-      xlabel = x_label if x_label else score
-      ylabel = y_label if y_label else "Term"
-      bar.set_xlabel(xlabel, fontsize=16, fontweight='bold')
-      bar.set_ylabel(ylabel, fontsize=16, fontweight='bold')
+        bar = dd.plot.barh(
+            y=score,
+            alpha=0.75,
+            color=colors,
+            fontsize=16,
+            ax=ax)
 
-      bar.legend_.remove()
-      bar.set_title(title, fontsize=16, fontweight='bold')
+        xlabel = x_label if x_label else score
+        ylabel = y_label if y_label else "Term"
+        bar.set_xlabel(xlabel, fontsize=16, fontweight='bold')
+        bar.set_ylabel(ylabel, fontsize=16, fontweight='bold')
 
-      plt.savefig(out_dir)     
-      
-      return ax
+        bar.legend_.remove()
+        bar.set_title(title, fontsize=16, fontweight='bold')
+
+        plt.savefig(out_dir)
+
+        return ax
 
     except KeyError:
-      print('ERROR: key \'' + str(score) + '\' is not in provided dataframe')
+        print('ERROR: key \'' + str(score) + '\' is not in provided dataframe')
+
 
 def gsea(
     input_gene_ranking_file: str,
@@ -119,16 +121,16 @@ def gsea(
     verbosity: Optional[bool] = False,
 
 ):
-  
+
     """\
-    
+
     Python interface for 2 preranked GSEA functions: gseapy (Fang, 2020) and fgsea (Korotkevich et al., 2019).
-    Users provide a set of genes to be considered, a set of enrichment terms 
-    (hallmark gene sets that are a defined library or set of pathways), 
-    and select either gseapy (default) or fgsea. 
-    The set of genes may be optionally pre-ranked with user-defined weights. 
-    The output is a table of results for each enrichment term. 
-    
+    Users provide a set of genes to be considered, a set of enrichment terms
+    (hallmark gene sets that are a defined library or set of pathways),
+    and select either gseapy (default) or fgsea.
+    The set of genes may be optionally pre-ranked with user-defined weights.
+    The output is a table of results for each enrichment term.
+
     Parameters
     ----------
     input_gene_ranking_file
@@ -146,22 +148,22 @@ def gsea(
         Required for fgsea
     verbosity
         Passed in to execute_r_script. Default false
-   
+
     Returns
     -------
     Pandas dataframe of GSEA results
-    
+
     Notes
     -------
     For hallmark_gene_sets, user must either provide a .gmt file or list of genes
-    
+
     Resulting table has columns of both gseapy and fgsea results, but may be NaN
     based on the type of GSEA chosen
 
     gseapy specific columns: 'fdr' (false discovery rate)
     fgsea specific columns: 'padj' (adjusted p-value), 'log2error'
 
-    For both fgsea and gseapy, the bounds for the minimum and maximum number 
+    For both fgsea and gseapy, the bounds for the minimum and maximum number
     of genes that are also in the gene set are as follows
     minsize = 15
     maxsize = 500
@@ -175,7 +177,7 @@ def gsea(
         raise ValueError(
             'You can\'t specify both a hallmark_gene_sets_file, hallmark_gene_sets_list. ' 'Please select only one.'
         )
-        
+
     if type == 'gseapy':
 
         import gseapy as gp
@@ -187,31 +189,34 @@ def gsea(
         hallmark_gene_sets = hallmark_gene_sets_list if hallmark_gene_sets_list else hallmark_gene_sets_file
 
         # run gseapy, returns a prerank object
-        pre_res = gp.prerank(rnk=rnk, gene_sets=hallmark_gene_sets, 
-                    no_plot=True,
-                    seed=0)
-        
+        pre_res = gp.prerank(
+            rnk=rnk,
+            gene_sets=hallmark_gene_sets,
+            no_plot=True,
+            seed=0
+        )
+
         gseapy_df = pre_res.res2d
 
         if out_dir is not None:
             gseapy_df.to_csv(out_dir, index=True)
 
         return gseapy_df
-        
+
     elif type == 'fgsea':
         # run fgsea
         print("fgsea")
 
         # create the arguments
         args = [input_gene_ranking_file]
-        
+
         if hallmark_gene_sets_file is not None:
             args += ['--file', hallmark_gene_sets_file]
         else:
             # modify the list to be read in as r input
             # r_list = ",".join(hallmark_gene_sets_list)
             args += ['--list', hallmark_gene_sets_list]
-        
+
         # execute R script
         # handles missing rscript_path error
         sce.tl.execute_r_script(rscript_path, './_scripts/fgsea.R', args, verbosity=verbosity)
@@ -223,4 +228,3 @@ def gsea(
         raise ValueError(
             'Please select either "fgsea" or "gseapy" (default).'
         )
-
--- scanpy/tests/external/test_gsea.py
+++ scanpy/tests/external/test_gsea.py
@@ -5,28 +5,30 @@ import pandas as pd
 import scanpy.external as sce
 from matplotlib.testing.compare import compare_images
 
-pytest.importorskip("gsea")
+# pytest.importorskip("gsea")
+
 
 def test_gsea_barplot():
     """
     Test that gsea_barplot works
     """
-    gseapy_df = pd.read_csv('.\_data\master_gseapy_df.csv')
-    sce.tl.gsea_barplot(gseapy_df, score = 'nes', color = ['b','r'], out_dir = '_images/test_gsea_barplot.png')
+    gseapy_df = pd.read_csv('./_data/master_gseapy_df.csv')
+    sce.tl.gsea_barplot(gseapy_df, score='nes', color=['b','r'], out_dir='_images/test_gsea_barplot.png')
     assert compare_images('_images/test_gsea_barplot.png', '_images/master_gsea_barplot.png', tol=5) is None
+
 
 def test_gseapy():
     """
     Test that gseapy module option for gsea() works
     """
-    sce.tl.gsea('.\_data\gsea_data.gsea_data.rnk', hallmark_gene_sets_list='KEGG_2016', type='gseapy', out_dir='_data/test_gseapy_df.csv')
+    sce.tl.gsea('./_data/gsea_data.gsea_data.rnk', hallmark_gene_sets_list='KEGG_2016', type='gseapy', out_dir='_data/test_gseapy_df.csv')
     master_gseapy_sample_df = pd.read_csv('_data/master_gseapy_df.csv')
     test_gseapy_sample_df = pd.read_csv('_data/test_gseapy_df.csv')
     assert test_gseapy_sample_df.equals(master_gseapy_sample_df)
+
 
 def test_fgsea(rscript_path):
     """
     Test that fgsea module option for gsea() works
     """
-    sce.tl.gsea('.\_data\gsea_data.gsea_data.rnk', hallmark_gene_sets_list='KEGG_2016', type='fgsea', out_dir='_data/test_fgsea_df.csv', rscript_path=rscript_path)
-
+    sce.tl.gsea('./_data/gsea_data.gsea_data.rnk', hallmark_gene_sets_list='KEGG_2016', type='fgsea', out_dir='_data/test_fgsea_df.csv', rscript_path=rscript_path)
