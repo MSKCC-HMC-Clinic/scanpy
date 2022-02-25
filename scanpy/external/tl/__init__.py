fc165a47c083e1745bce6acc96a1c38af73535ed
ek0212 <eve.kazarian@gmail.com>
2022-02-25 10:53:42 -0800

    submit cluster covariance to scanpy repo

--- scanpy/external/tl/__init__.py
+++ scanpy/external/tl/__init__.py
@@ -6,6 +6,4 @@ from ._trimap import trimap
 from ._harmony_timeseries import harmony_timeseries
 from ._sam import sam
 from ._wishbone import wishbone
-from ._joyplot import joyplot
-from ._adata_to_gene_expression_df import adata_to_gene_expression_df
-from ._joyplot import df_to_joyplot
+from ._cluster_covariance import cluster_covariance
