f42949ef01b40c08362475807b27f28372f9c295
Julia Qian <qjulia32@gmail.com>
2022-03-23 12:26:47 -0700

    clean and test gene visualization

--- scanpy/external/tl/_gene_visualization.py
+++ scanpy/external/tl/_gene_visualization.py
@@ -2,93 +2,88 @@ from typing import Union, Optional, List, Tuple, Sequence
 
 import numpy as np
 import pandas as pd
-import pandas as pd
 from anndata import AnnData
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
-from matplotlib.colors import Colormap, Normalize
-
-import phenograph
+from matplotlib.colors import Colormap
 import scipy
 
 
 def visualize_gene_signature(
     adata: AnnData,
     gene_signature: Sequence[str],
-    basis: str, #Literal['pca', 'tsne', 'umap', 'diffmap', 'draw_graph_fr']
-    visualization_type: str = 'zscore', # ['zscore', 'medoid']
-    # clusters: int = 1, # for 'medoid' visualization_type
+    basis: str,  # Literal['pca', 'tsne', 'umap', 'diffmap', 'draw_graph_fr']
+    visualization_type: str = 'zscore',
     cmap: Union[Colormap, str, None] = 'viridis',
     figsize: Optional[Tuple[float, float]] = (6.5,6),
     title: Optional[str] = None,
-    # save: Union[bool, str, None] = None, # TODO: follow scanpy save vs show logic
     out_pth: Optional[str] = None,
     return_fig: Optional[bool] = None,
 ) -> Union[Figure, Axes, None]:
 
     """\
-   
+
     Visualize a gene signature using the average z-score expression on the provided basis
 
 
     Parameters
     ----------
     adata
-        Annotated data matrix.
+        Annotated data matrix with obsm with basis data
     gene_signature
         List of gene names
     basis
         String that denotes a plotting tool that computed coordinates.
     visualization_type
-        String that denotes the values to plot for the color mapping. Options: 'zscore' (default), 'medoid'
+        String that denotes the values to plot for the color mapping. Options: 'zscore' (default). TODO: frechet mean
     title
         Figure title
     cmap
         Colormap name or sequence of strings
     return_fig
         Boolean return
-    
+
     Returns
     -------
     RIGHT NOW: Matplotlib axes object
 
-    TODO: If show==False a Axes or list of it    
-
     Example
     -------
-    >>> gene_signature = ['F13A1', 'VCAN', 'CCR2', 'ITGAL', 'CX3CR1', 'CD300E', 'ACE', 'S100A8', 'S100A9', 'CSF3R', 
+    >>> gene_signature = ['F13A1', 'VCAN', 'CCR2', 'ITGAL', 'CX3CR1', 'CD300E', 'ACE', 'S100A8', 'S100A9', 'CSF3R',
                      'IL1B', 'CLEC10A', 'CD74', 'FLT3', 'CSF1R']
     >>> visualize_gene_signature(adata=adata, gene_signature=gene_signature, basis='umap', return_fig=True)
 
     """
 
+    # clear axes and figure
+    plt.clf()
+    plt.cla()
+
     if basis not in ['pca', 'tsne', 'umap']:
-      raise ValueError('basis must be either: `pca`, `tsne`, or `umap`')
+        raise ValueError('basis must be either: `pca`, `tsne`, or `umap`')
 
     if visualization_type not in ['zscore', 'medoid']:
-      raise ValueError('visualization type must be either: `zscore` or `medoid`')
+        raise ValueError('visualization type must be either: `zscore` or `medoid`')
 
     basis_id = 'X_' + basis
     gene_ids = [adata.var_names.get_loc(j) for j in gene_signature]
 
     if visualization_type == 'zscore':
-      # first get zscores of each cell by genes in gene signature
-      avg_zs = np.mean(scipy.stats.zscore(adata.X[:, gene_ids]), axis = 1) # mean across rows of zscores
+        # first get zscores of each cell by genes in gene signature
+        avg_zs = np.mean(scipy.stats.zscore(adata.X[:, gene_ids]), axis=1)  # mean across rows of zscores
 
-      # axs = ax if ax else plt.figure(figsize=figsize)
-      c = avg_zs
-    # elif visualization_type == 'medoid':
-    #   kmedoids = KMedoids(n_clusters=clusters, random_state=0).fit(points)
-      
-    ax = plt.scatter(adata.obsm[basis_id][:, 0], adata.obsm[basis_id][:, 1], s = 2, c = c, 
-                    cmap = cmap) 
-    
+        # axs = ax if ax else plt.figure(figsize=figsize)
+        c = avg_zs
+
+    ax = plt.scatter(adata.obsm[basis_id][:, 0], adata.obsm[basis_id][:, 1], s=2, c=c,
+                     cmap=cmap)
+
     title = title if title else basis + ' colored by ' + visualization_type
-    plt.title(title, fontsize = 14)
+    plt.title(title, fontsize=14)
     plt.colorbar(ax)
     plt.axis('off')
     plt.savefig(out_pth, figsize=figsize)
 
     if return_fig:
-      return ax
+        return ax
