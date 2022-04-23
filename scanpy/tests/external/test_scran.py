5c590892cb98b28c9830083d7d9520535d7d4f3a
Julia Qian <qjulia32@gmail.com>
2022-04-22 21:21:48 -0700

    updated scran testing

--- scanpy/tests/external/test_scran.py
+++ scanpy/tests/external/test_scran.py
@@ -15,23 +15,15 @@ from scipy.sparse import csr_matrix
 
 # pytest.importorskip("scran")
 
-def test_scran_X_compressed(rscript_path):
-    """Test functionality using adata = sc.datasets.pbmc68k_reduced with input adata.X in compressed format"""
-    pbmc = sc.datasets.pbmc68k_reduced()
-    pbmc.X = csr_matrix(pbmc.X)
-    print(type(pbmc.X))
-    normed_adata = sce.tl.scran(pbmc, rscript_path, verbosity=True)
-    assert 'scran_norm' in normed_adata.layers
-    return normed_adata
 
-
-def test_scran_X_not_compressed(rscript_path):
-    """Test functionality using adata = sc.datasets.pbmc68k_reduced with input adata.X in non-compressed format
-       Test that scran() compresses the input automatically
+def test_scran_reduced(rscript_path):
+    """Test functionality using subsampled adata from pbmc10k.h5ad using
+        sc.pp.subsample(adata, fraction=0.10)
     """
-    pbmc = sc.datasets.pbmc68k_reduced()
-    normed_adata = sce.tl.scran(pbmc, rscript_path, verbosity=True)
-    assert 'scran_norm' in normed_adata.layers
+    datapath = "./_data/0.1-pbmc10k.h5ad"
+    adata = sc.read_h5ad(datapath)
+    normed_adata = sce.tl.scran(adata, rscript_path)
+    assert 'scran_norm' in normed_adata.layers 
     return normed_adata
 
 
@@ -39,39 +31,16 @@ def test_scran_layer(rscript_path):
     """Test functionality using adata = sc.datasets.pbmc68k_reduced with input adata.X in non-compressed format
        Test layer input
     """
-    pbmc = sc.datasets.pbmc68k_reduced()
-    pbmc.layers['raw_data'] = pbmc.X.copy()
-    normed_adata = sce.tl.scran(pbmc, rscript_path, layer="raw_data", verbosity=True)
+    datapath = "./_data/0.1-pbmc10k.h5ad"
+    adata = sc.read_h5ad(datapath)
+    adata.layers['raw_data'] = adata.X.copy()
+    normed_adata = sce.tl.scran(adata, rscript_path, layer="raw_data")
     assert 'scran_norm' in normed_adata.layers
     return normed_adata
 
 
 def test_scran_adata_read_path(rscript_path):
-    pbmc = sc.datasets.pbmc68k_reduced()
-    pbmc_temp_path = "./_data/pbmc_tmp.h5ad"
-    pbmc.write(pbmc_temp_path)
-    normed_adata = sce.tl.scran(None, rscript_path, adata_read_path=pbmc_temp_path, verbosity=True)
-    assert 'scran_norm' in normed_adata.layers    
-    return normed_adata
-
-
-def test_scran(rscript_path):
-
-    # make sure this is sparse and you need to change adata.X to whichever data the user wants 
-    # this or adata.layers['raw_data'] or something like that
-    datapath = "./_data/pbmc10k.h5ad"
-    adata = sc.read_h5ad(datapath)
-    adata.layers['raw_data'] = adata.X.copy()
-    normed_adata = sce.tl.scran(adata, rscript_path, verbosity=True, cache=True)
-
-    assert 'scran_norm' in normed_adata.layers 
-    return normed_adata
-
-
-def test_scran_reduced(rscript_path):
     datapath = "./_data/0.1-pbmc10k.h5ad"
-    adata = sc.read_h5ad(datapath)
-    normed_adata = sce.tl.scran(adata, rscript_path, cache=True)
-    print("in test, past scran")
-    assert 'scran_norm' in normed_adata.layers 
+    normed_adata = sce.tl.scran(None, rscript_path, adata_read_path=datapath)
+    assert 'scran_norm' in normed_adata.layers    
     return normed_adata
